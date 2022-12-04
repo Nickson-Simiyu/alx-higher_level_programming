b1df2d184cfc4c0aefa24207de74835dfcc460a4
Nickson-Simiyu <simiyunickson1@gmail.com>
2022-12-03 23:19:02 -0800

    else

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-for a in range(97, 97 + 26):
-    if a == 113 or a == 101:
-        continue
-    print("{:c}".format(a), end='')
+for letter in range(97, 123):
+    if chr(letter) != 'q' and chr(letter) != 'e':
+        print("{}".format(chr(letter)), end="")
