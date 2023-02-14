6a38f2b7a67828c1af6ae24c29ae32d2cfa23abc
Nickson-Simiyu <simiyunickson1@gmail.com>
2023-02-14 06:12:57 -0800

    sequel

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,5 @@
+-- This script creates the hbtn_0c_0 database in the MySQL server
+
+-- Create database if it does not exist
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
+
---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,5 @@
+-- This script deletes the hbtn_0c_0 database in the MySQL server
+
+-- Drop database if it exists
+DROP DATABASE IF EXISTS hbtn_0c_0;
+
