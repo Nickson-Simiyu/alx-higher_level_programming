f93e21516b6e2f4612448d26b105d54127889072
Nickson-Simiyu <simiyunickson1@gmail.com>
2023-03-14 04:01:22 -0700

    scripts

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,17 +1,14 @@
 #!/usr/bin/node
-const dict = require('./101-data').dict;
+const { dict } = require('./101-data.js');
 
-const totalist = Object.entries(dict);
-const vals = Object.values(dict);
-const valsUniq = [...new Set(vals)];
 const newDict = {};
-for (const j in valsUniq) {
-  const list = [];
-  for (const k in totalist) {
-    if (totalist[k][1] === valsUniq[j]) {
-      list.unshift(totalist[k][0]);
-    }
+
+for (const [userId, occurrences] of Object.entries(dict)) {
+  if (newDict[occurrences] === undefined) {
+    newDict[occurrences] = [userId];
+  } else {
+    newDict[occurrences].push(userId);
   }
-  newDict[valsUniq[j]] = list;
 }
+
 console.log(newDict);
--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,6 +1,13 @@
 #!/usr/bin/node
 const fs = require('fs');
 
-const fArg = fs.readFileSync(process.argv[2]).toString();
-const sArg = fs.readFileSync(process.argv[3]).toString();
-fs.writeFileSync(process.argv[4], fArg + sArg);
+const sourceFile1 = process.argv[2];
+const sourceFile2 = process.argv[3];
+const destFile = process.argv[4];
+
+const data1 = fs.readFileSync(sourceFile1);
+const data2 = fs.readFileSync(sourceFile2);
+const concatenatedData = Buffer.concat([data1, data2]);
+
+fs.writeFileSync(destFile, concatenatedData);
+console.log(`The contents of ${sourceFile1} and ${sourceFile2} have been concatenated to ${destFile}`);
