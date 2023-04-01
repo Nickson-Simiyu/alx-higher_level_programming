d15582d4d54c732a53773cc6b4b9764a92719b69
Nickson-Simiyu <simiyunickson1@gmail.com>
2023-04-01 06:10:59 -0700

    Refactor script to handle missing URL argument

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-"""A Python script that takes in a URL, 
-- sends a request to the URL and
-- displays the value of the X-Request-Id variable found in the header of the response.
+"""A Python script that takes in a URL, sends a request to the URL and displays the value of the X-Request-Id variable found in the header of the response.
 """
 import sys
 import urllib.request
 
 if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: {} URL".format(sys.argv[0]))
+        sys.exit(1)
+
     url = sys.argv[1]
 
     request = urllib.request.Request(url)
