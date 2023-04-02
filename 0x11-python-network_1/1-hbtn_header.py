46ffb7fac48104b0727fd9f5428c62ea047eb838
Nickson-Simiyu <simiyunickson1@gmail.com>
2023-04-02 04:40:39 -0700

    updated

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
-"""A Python script that takes in a URL, sends a request to the URL and displays the value of the X-Request-Id variable found in the header of the response.
-"""
+"""A Python script that takes in a URL, sends a request to the URL and displays the value of the X-Request-Id variable found in the header of the response."""
 import sys
-import urllib.request
+import urllib.request as urllib
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("Usage: {} URL".format(sys.argv[0]))
-        sys.exit(1)
 
-    url = sys.argv[1]
+    """Get url first from arguments list"""
+    myurl = sys.argv[1]
 
-    request = urllib.request.Request(url)
-    with urllib.request.urlopen(request) as response:
+    request = urllib.Request(myurl)
+
+    """Open the response encoded"""
+    with urllib.urlopen(request) as response:
         print(dict(response.headers).get("X-Request-Id"))
