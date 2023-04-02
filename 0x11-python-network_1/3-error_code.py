5be62ac0af35f82b186054eae2c7939bcb1b40a7
Nickson-Simiyu <simiyunickson1@gmail.com>
2023-04-02 10:30:16 -0700

    updated and made executable

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-"""A Python script that takes in a URL, sends a request to the URL and displays the body of the response (decoded in utf-8)."""
+"""Sends a request to a given URL and displays the response body.
+Usage: ./3-error_code.py <URL>
+  - Handles HTTP errors.
+"""
 
 if __name__ == "__main__":
     import sys
